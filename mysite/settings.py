0e9b9d2a340d1fb555018911f1eaa9b1441a415a
dion21001 <simbolonrikki12gmail.com>
2026-06-14 18:03:14 +0700

    menambahkan database

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'RDSPERCOBAAN',
+        'USER': 'admin',
+        'PASSWORD': 'Pramuka123',
+        'HOST': 'database-1.cg84keca9azq.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
